e4f6817a896ad219cc1f2fd2687fdddac6b7fc8c
MartinKulic <kulich@stud.uniza.sk>
2026-09-20 23:52:33 +0200

    Add listeners after DOM Content Loaded

--- frontend/map/script.js
+++ frontend/map/script.js
@@ -1,26 +1,28 @@
-sellected_collection = document.getElementById("select-collection").value;
-
-map_controller = new MapController(sellected_collection)
-
 document.addEventListener('DOMContentLoaded', function() {
+    const sellected_collection = document.getElementById("select-collection").value;
+    const map_controller = new MapController(sellected_collection)
+
     map_controller.fit_bounds(initBounds)
     map_controller.fit_slider()
 
     map_controller.set_ith_tiff(document.getElementById("timeserie-slider").value)
+
+
+
+    // input  - Fires continuously while dragging
+    // change - Fires when dragging stops or focus is lost
+    document.getElementById("timeserie-slider").addEventListener("input", function(event) {
+        let new_value = event.target.value;
+        map_controller.set_ith_tiff(new_value)
+    })
+
+    document.getElementById("opacity-slider").addEventListener("input", function(event) {
+        let new_value = event.target.value;
+        map_controller.set_opacity(new_value)
+    })
+
+    document.getElementById("cache-all-button").addEventListener("click", function(event) {
+        map_controller.add_all_layers()
+    })
 });
 
-// input  - Fires continuously while dragging
-// change - Fires when dragging stops or focus is lost
-document.getElementById("timeserie-slider").addEventListener("input", function(event) {
-    let new_value = event.target.value;
-    map_controller.set_ith_tiff(new_value)
-})
-
-document.getElementById("opacity-slider").addEventListener("input", function(event) {
-    let new_value = event.target.value;
-    map_controller.set_opacity(new_value)
-})
-
-document.getElementById("cache-all-button").addEventListener("click", function(event) {
-    map_controller.add_all_layers()
-})
